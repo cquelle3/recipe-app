9a28970fa99dac0c607192aa8e964df68bdd46a1
Colin Quelle <cmq1999@yahoo.com>
2023-06-26 20:09:30 -0400

    Created sample react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
 
+function ItemRow({ item }) {
+  return (
+    <tr>
+      <td>{item.name}</td>
+      <td>{item.price}</td>
+    </tr>
+  );
+}
+
+function ItemTable({ items }) {
+  const rows = [];
+
+  items.forEach((item) => {
+    rows.push(
+      <ItemRow item={item} key={item.name} />
+    );
+  });
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        {rows}
+      </tbody>
+    </table>
+  );
+}
+
+const ITEMS = [
+  {name: "Cheerios", price: "5$"},
+  {name: "Apple", price: "1$"},
+  {name: "Milk", price: "4$"}
+];
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ItemTable items={ITEMS}/>
+  )
 }
 
 export default App;
